a9d92bd0bc8eb9c02dbb2206a1449c1b720a2e60
ryansnorek <32182282+ryansnorek@users.noreply.github.com>
2021-06-16 11:15:07 -0700

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,30 @@
+# ---------------------------------------------------------------
+# Variables for importing
+# ---------------------------------------------------------------
+# Shared model variables
+price = 21.76
+eps = 0.28
+dividend = 0.27
+targetRateOfReturn = 0.12
+PEatSale = 17
+
+# Graham-Lynch model
+longTermGrowthRate = 0.12
+corporateAAABondYield = 0.04
+
+# Simple model
+shortTermEPSgrowth = 0.25
+longTermEPSgrowth = 0.1
+shortTermDividendGrowth = 0.048
+longTermDividendGrowth = 0.03
+
+# Detailed model
+volumeGrowthRate = 0.36
+pricingGrowthRate = 0.03
+pmExpansionRate = 0
+shareReductionPercentPerYear = 0.03
+dividendPayoutChangeRate = 0
+
+# Empty objects for storing projections
+epsProjections = {}
+dividendProjections = {}
